3f40d9685ed607556222163e3dcceeffa6f46ae6
Nicolás Potier <nicolaspotier97@gmail.com>
2021-07-28 00:37:11 -0500

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppComponent } from './app.component';
 import { BoostwachModule } from './core/boostwach.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
